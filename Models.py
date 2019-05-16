cff365047c3729f1dbbe6b6049bad0d71ade2f02
Kaynelua <luayinghao95@hotmail.com>
2019-05-16 14:12:27 +0100

    Text to speech module implemented on python

--- Models.py
+++ Models.py
@@ -1,41 +1,28 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers.recurrent import LSTM
-from keras.layers import Dense,Dropout,MaxPooling3D,ZeroPadding3D
-from keras.layers import LeakyReLU,ReLU,Conv3D,Flatten,Conv2D,Lambda
-from keras.optimizers import Adam,SGD
-import keras.backend as K
-from keras.layers import Dense,Dropout,Conv3D,Input,MaxPool3D,Flatten,Activation
-from keras.regularizers import l2
-from keras.models import Model
-from keras.applications.mobilenet_v2 import MobileNetV2
-
 import tensorflow as tf
 
+import keras.backend as K
 
-def seqlstm():
-    """Build a simple LSTM network. We pass the extracted features from
-    our CNN to this model predomenently."""
-    # Model .
-    model = Sequential()
-    model.add(Lambda(lambda x: K.l2_normalize(x,axis=-1), input_shape= (30,1280)))
-    model.add(LSTM(512, return_sequences=True,
-                   input_shape= (30,1280), #1280 for mobilenet, 2048 for inception and exception
-                   dropout=0.5))
-    model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(6, activation='softmax'))
-    model.add(Lambda(lambda x : tf.keras.backend.mean(x[:,25:,:],axis=1)))
+from keras.models import Sequential
+from keras.models import Model
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(),
-                  metrics=['accuracy'])
-    return model
-    
+from keras.layers import Input,Flatten,Dense,Dropout,Activation,Reshape
+from keras.layers import Conv2D
+from keras.layers import Conv3D,MaxPooling3D,ZeroPadding3D,MaxPool3D
+from keras.layers import LeakyReLU,ReLU,Lambda
+from keras.optimizers import Adam,SGD
+from keras.layers.recurrent import LSTM
+from keras.regularizers import l2
+
+from keras.applications.mobilenet_v2 import MobileNetV2
+
+
 def lstm():
-    """Build a simple LSTM network. We pass the extracted features from
-    our CNN to this model predomenently."""
-    # Model .
+    """
+    Simple LSTM network. 
+    Pass the extracted features from CNN to this model.
+    Perform L2 normalisation to input feature vector
+    """
     model = Sequential()
     model.add(Lambda(lambda x: K.l2_normalize(x,axis=-1), input_shape= (30,1280)))
     model.add(LSTM(512, return_sequences=False,
@@ -50,12 +37,33 @@ def lstm():
                   metrics=['accuracy'])
     return model
 
+
+def seqlstm():
+    """
+    Simple LSTM network.
+    Output for each LSTM unit is used and average is used as prediction
+    Pass the extracted features from CNN to this model.
+    Perform L2 normalisation to input feature vector
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: K.l2_normalize(x,axis=-1), input_shape= (30,1280)))
+    model.add(LSTM(512, return_sequences=True,
+                   input_shape= (30,1280), #1280 for mobilenet, 2048 for inception and exception
+                   dropout=0.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(6, activation='softmax'))
+    model.add(Lambda(lambda x : tf.keras.backend.mean(x[:,25:,:],axis=1)))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
+    return model
+
+
 def c3d_sports():
 
-    if K.image_data_format() == 'channels_last':
-        shape = (16,112,112,3)
-    else:
-        shape = (3,16,112,112)
+    shape = (16,112,112,3)
 
     model = Sequential()
     model.add(Conv3D(64, 3, activation='relu', padding='same', name='conv1', input_shape=shape))
@@ -84,16 +92,60 @@ def c3d_sports():
     model.add(Dense(4096, activation='relu', name='fc7'))
     model.add(Dropout(0.5))
     model.add(Dense(487, activation='softmax', name='fc8'))
-
-    model.load_weights('sports1M_weights_tf.h5')    
-    for i in range(len(model.layers)):
-      model.layers[i].trainable = False
-
-    model.pop()
-    model.add(Dense(2, activation='softmax', name='fc8'))
-    model.compile(loss='categorical_crossentropy',
-            optimizer=Adam(),
-            metrics=['accuracy'])
+    
+    model.load_weights('pretrained_weights/sports1M_weights_tf.h5')   
 
     return model
 
+def c3d():
+
+    pre_train = c3d_sports()
+
+    shape = (30,112,112,3)
+
+    model = Sequential()
+    model.add(Conv3D(64, 3, activation='relu', padding='same', name='conv1', input_shape=shape))
+    model.add(MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), padding='same', name='pool1'))
+    
+    model.add(Conv3D(128, 3, activation='relu', padding='same', name='conv2'))
+    model.add(MaxPooling3D(pool_size=(2,2,2), strides=(2,2,2), padding='valid', name='pool2'))
+    
+    model.add(Conv3D(256, 3, activation='relu', padding='same', name='conv3a'))
+    model.add(Conv3D(256, 3, activation='relu', padding='same', name='conv3b'))
+    model.add(MaxPooling3D(pool_size=(2,2,2), strides=(2,2,2), padding='valid', name='pool3'))
+    
+    model.add(Conv3D(512, 3, activation='relu', padding='same', name='conv4a'))
+    model.add(Conv3D(512, 3, activation='relu', padding='same', name='conv4b'))
+    model.add(MaxPooling3D(pool_size=(2,2,2), strides=(2,2,2), padding='valid', name='pool4'))
+    
+    model.add(Conv3D(512, 3, activation='relu', padding='same', name='conv5a'))
+    model.add(Conv3D(512, 3, activation='relu', padding='same', name='conv5b'))
+    model.add(ZeroPadding3D(padding=(0,1,1)))
+    model.add(MaxPooling3D(pool_size=(2,2,2), strides=(2,2,2), padding='valid', name='pool5'))
+    
+    model.add(Reshape((16,512)))
+
+    model.add(Lambda(lambda x: K.l2_normalize(x,axis=-1)))
+    model.add(LSTM(512, return_sequences=False,
+                   input_shape= (16,512),
+                   dropout=0.5))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(6, activation='softmax'))
+
+    for i in range(0,14,1):
+        model.layers[i].set_weights(pre_train.layers[i].get_weights())
+
+    for i in range(0,7,1):    
+        model.layers[i].trainable = False
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
+
+    return model
+
+
+
+    # for i in range(len(model.layers)):
+    #   model.layers[i].trainable = False
--- training.py
+++ training.py
@@ -1,30 +1,66 @@
 import os
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import keras
 
 from keras.preprocessing import image
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import OneHotEncoder
-from Models import lstm,c3d_model,c3d_sports,fcn,seqlstm
-from tqdm import tqdm
 from keras.utils import to_categorical
 
-df = pd.read_csv( 'jester-v4-train-five.csv',index_col = None,header=None,sep=';') #v1 for 4 classes, v2 for 6
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+
+from tqdm import tqdm
+
+from Models import lstm,seqlstm,c3d_sports,c3d
 
 class DataGenerator(keras.utils.Sequence):
-
 	def __init__(self, df,  batch_size=32, num_frames = 30, dim=2048, n_channels=1, shuffle=True): 
-		#dim = 2048 for inception, 1280 for mobilenet
-	    # 'Initialization'
+	    # Initialization
 		self.transform = None
 		self.dim = dim
 		self.batch_size = batch_size
 		self.n_channels = n_channels
 		self.shuffle = shuffle
 		self.df = df
-		self.num_frames = num_frames
+		self.num_frames  = num_frames
+		self.num_classes = len(df['Action'].unique())
 		self.on_epoch_end()
+
+	def __len__(self):
+		'''Denotes the number of batches per epoch'''
+		return int(np.floor(self.df.shape[0]/self.batch_size))
+
+	def __getitem__(self, index):
+		'''Returns Training Data for a Single Batch'''
+		y = []
+		df_batch = self.df[index*self.batch_size:(index+1)*self.batch_size]
+		X =  np.empty((self.batch_size , self.num_frames , self.dim), dtype=np.uint8)
+		path = 'jester-data/jester-features/'
+		for i in range (0,self.batch_size,1):
+			v,label,num_frames = df_batch.iloc[i]
+			X[i] = np.load(path+str(v)+"-"+"features-mobilenet" + ".npy")[:self.num_frames]
+		y = to_categorical(df_batch['Action'].values,num_classes=self.num_classes)
+		return X,y
+			
+	def on_epoch_end(self):
+		'''Updates indexes after each epoch'''
+		self.df = self.df.sample(frac=1.0)
+
+
+class DataGeneratorF(keras.utils.Sequence):
+	def __init__(self, df,  batch_size=32, num_frames = 30, dim=(112,122), n_channels=3, shuffle=True): 
+	    # Initialization
+		self.transform = None
+		self.dim = dim
+		self.batch_size = batch_size
+		self.n_channels = n_channels
+		self.shuffle = shuffle
+		self.df = df
+		self.num_frames  = num_frames
+		self.num_classes = len(df['Action'].unique())
+		self.path = os.path.join('jester-data','20bn-jester-v1')
+		self.on_epoch_end()
+
 	def __len__(self):
 		'''Denotes the number of batches per epoch'''
 		return int(np.floor(self.df.shape[0]/self.batch_size))
@@ -32,77 +68,83 @@ class DataGenerator(keras.utils.Sequence):
 	def __getitem__(self, index):
 		y = []
 		df_batch = self.df[index*self.batch_size:(index+1)*self.batch_size]
-		X =  np.empty((self.batch_size , self.num_frames , self.dim), dtype=np.uint8)
+		X =  np.empty((self.batch_size,self.num_frames,) + self.dim + (self.n_channels,), dtype=np.uint8)
 		for i in range (0,self.batch_size,1):
 			v,label,num_frames = df_batch.iloc[i]
-			path = 'jester-data/jester-features/'+str(v)
-			frame3d = np.load(path+"-"+"features-mobilenet" + ".npy")[:self.num_frames]
-			X[i] = frame3d
-			y.append(label)
-		
-		label_encoder = LabelEncoder()
-		y = label_encoder.fit_transform(y)	
-		return X,to_categorical(y,num_classes=6)
+			folder_path = self.path+"/"+str(v)	
+			files = np.sort(np.array([os.path.splitext(filename)[0] for filename in os.listdir(folder_path)]))
+			files = files[:self.num_frames]
+			X[i] = [image.img_to_array(image.load_img(folder_path+"/"+str(f)+".jpg", target_size=self.dim)) for f in files]
+		y = to_categorical(df_batch['Action'].values,num_classes=self.num_classes)
+		return X,y
 			
 	def on_epoch_end(self):
 		# 'Updates indexes after each epoch'
 		self.df = self.df.sample(frac=1.0)
 
-def get_training_data():
-	df = pd.read_csv( 'jester-v4-train-five.csv',index_col = None,header=None,sep=';')
-	df.columns = ['Folder','Action','Frames']
-	mask = (df['Frames']>=16) & ((df['Action']=='Swiping Left') | (df['Action']=='Swiping Up') |
-								 (df['Action']=='Thumb Up')     | (df['Action']=='No gesture') |
-								 (df['Action']=='Rolling Hand Backward') | (df['Action']=='Zooming Out With Full Hand'))
-	df = df[mask].head(12000)
-	df = df.values
-	
 
-	X =  np.empty((12000,16,112,112,3),dtype=np.uint8)
+def get_training_data(df):
+	num_classes = len(df['Action'].unique())
+	df = df.head(6000)
+	df = df.values
+	X =  np.empty((6000,30,112,112,3),dtype=np.uint8)
 	y = []
-	pbar = tqdm(total=12000)
-	for i in range(0,12000,1):
+	pbar = tqdm(total=6000)
+	for i in range(0,6000,1):
 		v,label,num_frames= df[i]
 		path = os.path.join('jester-data','20bn-jester-v1',str(v))
 		files = np.sort(np.array([os.path.splitext(filename)[0] for filename in os.listdir(path)]))
-		skip = len(files) // 16
-		output = [files[i] for i in range(0, len(files), skip)]
-		files = output[:16]
+		files = files[:30]
 		X[i,] = [ image.img_to_array(image.load_img(path+"/"+f+".jpg", target_size=(112, 112))) for f in files]
 		pbar.update(1)
 	pbar.close()
-
-	label_encoder = LabelEncoder()
-	integer_encoded = label_encoder.fit_transform(df[:,1])
-	onehot_encoder = OneHotEncoder(sparse=False)
-	integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-	y = onehot_encoder.fit_transform(integer_encoded)
+	y = to_categorical(df[:,1],num_classes=num_classes)
 	return X,y
 
 
-df.columns = ['Folder','Action','Frames']
-mask = (df['Frames']>=30) & 	((df['Action']=='Swiping Left') | (df['Action']=='Swiping Down') |
-								(df['Action']=='Thumb Up')     | (df['Action']=='No gesture') |
-								(df['Action']=='Rolling Hand Backward') | (df['Action']=='Zooming Out With Full Hand') )
+
+df = pd.read_csv( 'jester-train.csv',
+					index_col = None,
+					header=None,
+					sep=';',
+					names=['Folder','Action','Frames'])
 
 
-df = df[mask].head(40000)
+mask = (df['Frames']>=30) & ((df['Action']=='Swiping Left') | (df['Action']=='Swiping Down') |
+							(df['Action']=='Thumb Up')      | (df['Action']=='No gesture') |
+							(df['Action']=='Rolling Hand Backward') | (df['Action']=='Zooming Out With Full Hand') )
+df = df[mask]
+
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(df['Action'])
+df['Action'] = integer_encoded
+
 dftrain = df.head(int(len(df)*0.8))
-dfval = df.tail(int(len(df)*0.2))
+dfval   = df.tail(int(len(df)*0.2))
 
-model = seqlstm()
-model.summary()
+# model = lstm()
+# model.summary()
 
-model.fit_generator(
-	DataGenerator(dftrain,dim=1280),
-	validation_data=DataGenerator(dfval,dim=1280),
-	verbose=1,
-	epochs=100,
-)
+# model.fit_generator(
+# 	DataGenerator(dftrain,dim=1280),
+# 	validation_data=DataGenerator(dfval,dim=1280),
+# 	verbose=1,
+# 	epochs=100,
+# )
 
 # model = c3d_sports()
+model = c3d()
+model.summary()
+model.fit_generator(
+	DataGeneratorF(dftrain,dim=(112,112)),
+	validation_data=DataGeneratorF(dfval,dim=(112,112)),
+	verbose=1,
+	epochs=100,
+	use_multiprocessing=True,
+	workers=8
+)
 
-# X,y = get_training_data()
+# X,y = get_training_data(df)
 # model.fit(X,
 #  		  y,
 #  		  verbose=1,
