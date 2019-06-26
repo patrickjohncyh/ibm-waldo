db600576a3b71844c270f034d9afe3a7534ea07b
Kaynelua <luayinghao95@hotmail.com>
2019-06-26 11:21:33 +0100

    Add audio_files and audio generation to work with naming differences

--- 3-Pi/pi-execution/IBM_text_to_speech.py
+++ 3-Pi/pi-execution/IBM_text_to_speech.py
@@ -1,6 +1,7 @@
 from ibm_watson import TextToSpeechV1
 import json
 import os
+from subprocess import call
 
 #Get ibm-watson(3.0.4) using pip3 : pip3 install ibm-watson
 #Ensure aplay is available on the system
@@ -10,7 +11,7 @@ text_to_speech = TextToSpeechV1(
     url='https://gateway-lon.watsonplatform.net/text-to-speech/api'
 )
 
-predefined_actions = ["I would like a coffee", "I would like to go out", "I would like to go on a bus", "I want to go home", "I am happy today", "Please stand further from me", "Please stand closer to me", "I'm ready"," "] 
+predefined_actions = ["I would like a coffee", "I would like to go out", "I would like to go on a bus", "I want to go home", "I am happy today", "Please stand further from me", "Please stand closer to me", "I'm ready"," ","Dinner","Good","Home","No","Sorry"] 
 #Add phrase here, audio files are numbered in order of this list
 
 for idx,action in enumerate(predefined_actions):
@@ -22,3 +23,14 @@ for idx,action in enumerate(predefined_actions):
                 accept='audio/wav'        
             ).get_result().content)
     os.system('aplay ' + 'audio_files/'+str(idx)+'.wav')
+
+print("Renaming 9.wav to Dinner.wav")
+call(["mv", "audio_files/9.wav", "audio_files/Dinner.wav"])
+print("Renaming 10.wav to Good.wav")
+call(["mv", "audio_files/10.wav", "audio_files/Good.wav"])
+print("Renaming 11.wav to Home.wav")
+call(["mv", "audio_files/11.wav", "audio_files/Home.wav"])
+print("Renaming 12.wav to No.wav")
+call(["mv", "audio_files/12.wav", "audio_files/No.wav"])
+print("Renaming 13.wav to Sorry.wav")
+call(["mv", "audio_files/13.wav", "audio_files/Sorry.wav"])
